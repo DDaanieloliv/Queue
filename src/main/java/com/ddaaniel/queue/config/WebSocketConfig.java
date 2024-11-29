889e9d8c259ac1fdeb59fb07e8399d07bb4e3e65
daniel <daniel0333v@gmail.com>
2024-11-29 15:29:31 -0300

    ...

--- src/main/java/com/ddaaniel/queue/config/WebSocketConfig.java
+++ src/main/java/com/ddaaniel/queue/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/fila-websocket")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("http://localhost:3000")
                 .withSockJS(); // Endpoint WebSocket com SockJS fallback
     }
 
